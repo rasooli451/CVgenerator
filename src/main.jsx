d6e881b75011148e9ad827393bc237865b00ea47
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-04-13 10:44:10 -0400

    inserted top.jsx instead of App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Top from './Top.jsx'
+import Information from './Components/Information.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Top />
   </StrictMode>,
 )
