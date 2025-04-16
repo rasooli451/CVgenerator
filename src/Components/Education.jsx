68e18550eb5922b83b1e0e914922f65e4050238c
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-04-16 19:59:02 -0400

    removed labels and added show state

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -12,27 +12,32 @@ import { useState } from "react";
 
 
 
-export default function Education(){
+export default function Education({onsave}){
+    const [show, setShow] = useState(true);
     const [school, setSchool] = useState("");
     const [degree, setDegree] = useState("");
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
-    const[background, setBackground] = useState("white");
-    const[fontColor, setFontColor] = useState("black");
+    const [style, setStyle] = useState({background : "white", color : "black"});
 
 
+    function toggleShow(){
+        setShow(!show);
+    }
 
     function handleChange(callback, e){
         callback(e.target.value);
     }
 
     function handleBackground(e){
-        setBackground(e.target.value);
-        setFontColor(e.target.fontColor);
+        let intel = e.target.value.split(",");
+        setStyle({background : intel[0], color : intel[1]})
     }
 
     function dealWithData(){
-        
+        if (school.length > 0 && degree.length > 0 && startDate.length > 0 && endDate.length > 0){
+            onsave({school, degree, startDate, endDate, style});
+        }
     }
 
     function clearEverything(){
@@ -40,36 +45,32 @@ export default function Education(){
         setDegree("");
         setStartDate("");
         setEndDate("");
-        setBackground("white");
-        setFontColor("black");
+        setStyle({background : "white", color : "black"});
     }
-    return <form className="Education section">
+    return !show ? <form className="Education section"><button className="toggle" type="button" onClick={toggleShow}>{school == "" ? "School Name" : school}</button></form> : <form className="Education section">
+        <button className="toggle" type="button" onClick={toggleShow}>{school == "" ? "School Name" : school}</button>
         <div className="formPart">
-            <label for="school">School: </label>
-            <input type="text" required id="school" name="school" value={school} onChange={(e)=> handleChange(setSchool, e)}/>
+            <input type="text" required id="school" name="school" placeholder="your school..." value={school} onChange={(e)=> handleChange(setSchool, e)}/>
             </div>
             <div className="formPart">
-            <label for="degree">Degree: </label>
-            <input type="text" required id="degree" name="degree" value={degree} onChange={(e)=> handleChange(setDegree, e)}/>
+            <input type="text" required id="degree" name="degree" placeholder="your degree..."value={degree} onChange={(e)=> handleChange(setDegree, e)}/>
             </div>
             <div className="formPart">
-            <label for="startDate">Start Date: </label>
-            <input type="text" required id="startDate" name="startDate" value={startDate} onChange={(e)=> handleChange(setStartDate, e)}/>
+            <input type="text" required id="startDate" name="startDate" placeholder="the start date..." value={startDate} onChange={(e)=> handleChange(setStartDate, e)}/>
             </div>
             <div className="formPart">
-            <label for="endDate">End Date: </label>
-            <input type="text" required id="endDate" name="endDate" value={endDate} onChange={(e)=> handleChange(setEndDate, e)}/>
+            <input type="text" required id="endDate" name="endDate" placeholder="the end date..." value={endDate} onChange={(e)=> handleChange(setEndDate, e)}/>
             </div>
             <div className="formPart">
-             <label for="background">Background Color: </label>
-             <select name="background" id="background" value={background} fontColor={fontColor} onChange={handleBackground}>
-                <option value="white" selected fontColor="black">White</option>
-                <option value="black" fontColor="white">Black</option>
-                <option value="blue" fontColor="black">Blue</option>
-                <option value="yellow" fontColor="black">Yellow</option>
-                <option value="purple" fontColor="white">Purple</option>
-                <option value="green" fontColor="black">Green</option>
-                <option value="red" fontColor="yellow">Red</option>
+             <label htmlFor="background">Background Color: </label>
+             <select name="background" id="background"  onChange={handleBackground}>
+                <option value="white,black" selected >White</option>
+                <option value="black,white" >Black</option>
+                <option value="blue,black" >Blue</option>
+                <option value="yellow,black" >Yellow</option>
+                <option value="purple,white" >Purple</option>
+                <option value="green,black" >Green</option>
+                <option value="red,yellow" >Red</option>
              </select>
              </div>
              <div className="formPart">
