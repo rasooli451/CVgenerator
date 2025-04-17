98c639b8a39f1a8273af1a2593d6d3388728f549
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-04-16 20:01:16 -0400

    finished up forms, started working on results

--- src/Top.jsx
+++ src/Top.jsx
@@ -9,18 +9,49 @@ import Education from "./Components/Education";
 
 import Experience from "./Components/Experience";
 
+import Informationui from "./Components/Informationui";
+
+import Educationui from "./Components/Educationui";
+
 import "./top.css";
+import { useState } from "react";
 
 
 export default function Top(){
+    //arguments to pass to each form component, a function to pass objects when they are saved in forms
+    const [information, setInformation] = useState(null);
+    const [education, setEducation] = useState(null);
+    const [experience, setExperience] = useState(null);
+    const [showEducation, setShowEducation] = useState(true);
+
+
+    function hideComponent(obj, callback){
+        callback(!obj);
+    }
+
+    function handleChange(obj, callback){
+        callback(obj);
+    }
+
+
+    
     return <>
     <div className="forms">
-    <Information />
-    <Education />
-    <Experience />
+    
+    <Information onsave={(obj)=> handleChange(obj, setInformation)}/>
+    <button className="hideComp" type="button" onClick={()=> hideComponent(showEducation, setShowEducation)}>Education</button>
+    {showEducation ? <Education onsave={(obj)=> handleChange(obj, setEducation)}/> : <Education onsave={(obj)=> handleChange(obj, setEducation)} className="hide"/>}
+    <Experience onsave={(obj)=> handleChange(obj, setExperience)}/>
     </div>
     <div className="result">
-        <h1>Enter and Save to Show the Result</h1>
+        <div className="information sect" style={information == null ? null : information.style}>
+            <Informationui obj={information}/>
+        </div>
+        <div className="education sect" style={education == null ? null : education.style}>
+            <h1>Education</h1>
+            <Educationui obj={education}/>
+        </div>
+        <div className="experience sect"></div>
     </div>
     </>
 
