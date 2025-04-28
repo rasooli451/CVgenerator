b2a0f84b64a6895f8b8b08a712ff0dc5f1faf24d
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-04-28 19:36:17 -0400

    took gpa into account and added it to the result section

--- src/Components/Educationui.jsx
+++ src/Components/Educationui.jsx
@@ -12,10 +12,11 @@
 
 export default function Educationui({obj}){
     return <div className="educationsect" style={obj.style}>
-        <p>{obj.startDate} - {obj.endDate}</p>
+        <p>{obj.startDate} / {obj.endDate}</p>
         <div className="schoolinfo">
             <h3>{obj.school}</h3>
             <p>{obj.degree}</p>
+            <p>GPA : {obj.gpa}</p>
         </div>
     </div>
 }
