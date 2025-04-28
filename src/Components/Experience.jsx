814b136166054f5978e25246f7e4e1c9e0b48be1
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-04-28 19:37:19 -0400

    the final version of Experience.jsx

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -8,30 +8,31 @@ import { useState } from "react";
 
 
 
-export default function Experience(){
-    const [show, setShow] = useState(true);
+export default function Experience({onsave, id, ondelete}){
+    const [show, setShow] = useState(false);
     const [companyName, setCompanyName] = useState("");
     const [title, setTitle] = useState("");
     const[startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
     const [description, setDescription] = useState("");
-    const [background, setBackground] = useState("white");
-    const [fontColor, setFontColor] = useState("black");
+    const [style, setStyle] = useState({background : "white", color : "black"});
 
+
+    
 
     function handleChange(callback, e){
         callback(e.target.value);
-        
     }
 
     function handleBackground(e){
-        setBackground(e.target.value);
-        setFontColor(e.target.fontColor);
+        let intel = e.target.value.split(",");
+        setStyle({background : intel[0], color : intel[1]})
     }
 
     function dealWithData(){
         if (companyName.length > 0 && title.length > 0 && startDate.length > 0 && endDate.length > 0 && description.length > 0){
             //send an object with all of the information to parent
+            onsave({companyName, title, startDate, endDate, description, id, style});
         }
     }
     function clearEverything(){
@@ -40,16 +41,19 @@ export default function Experience(){
         setStartDate("");
         setEndDate("");
         setDescription("");
-        setBackground("white");
-        setFontColor("black");
+        setStyle({background : "white", color : "black"});
     }
 
     function toggleShow(){
         setShow(!show);
     }
 
-    return !show ? <form className="section"><button className="toggle btn" type="button" onClick={toggleShow}>Experience</button></form> : <form className="Experience section">
-        <button className="toggle btn" type="button" onClick={toggleShow}>Experience</button>
+    function destroySelf(){
+        ondelete(id);
+    }
+
+    return !show ? <form className="entry"><button className="toggle btn" type="button" onClick={toggleShow}>{companyName == "" ? "Company Name" : companyName}</button></form> : <form className="entry">
+        <button className="toggle btn" type="button" onClick={toggleShow}>{companyName == "" ? "Company Name" : companyName}</button>
         <div className="formPart">
             <input id="companyName" name="companyName" placeholder="name of company..."required value={companyName} onChange={(e)=>handleChange(setCompanyName, e)}/>
         </div>
@@ -63,23 +67,24 @@ export default function Experience(){
             <input id="endDate" name="endDate" required placeholder="the end date..."value={endDate} onChange={(e)=>handleChange(setEndDate, e)}/>
         </div>
         <div className="formPart">
-            <textarea name="description" id="description" placeholder="description of job..."onChange={(e)=>handleChange(setDescription, e)} value={description}></textarea>
+            <textarea name="description" id="description" placeholder="Responsibilites separated by dot..."onChange={(e)=>handleChange(setDescription, e)} value={description}></textarea>
         </div>
         <div className="formPart">
-             <label htmlFor="background">Background Color: </label>
-             <select name="background" id="background" value={background} fontcolor={fontColor} onChange={handleBackground}>
-                <option value="white" selected fontcolor="black">White</option>
-                <option value="black" fontcolor="white">Black</option>
-                <option value="blue" fontcolor="black">Blue</option>
-                <option value="yellow" fontcolor="black">Yellow</option>
-                <option value="purple" fontcolor="white">Purple</option>
-                <option value="green" fontcolor="black">Green</option>
-                <option value="red" fontcolor="yellow">Red</option>
+        <label htmlFor="background">Background Color: </label>
+             <select name="background" id="background"  onChange={handleBackground}>
+                <option value="white,black" selected >White</option>
+                <option value="black,white" >Black</option>
+                <option value="blue,black" >Blue</option>
+                <option value="yellow,black" >Yellow</option>
+                <option value="purple,white" >Purple</option>
+                <option value="green,black" >Green</option>
+                <option value="red,yellow" >Red</option>
              </select>
              </div>
              <div className="formPart">
                   <button type="button" onClick={dealWithData} className="btn">Save</button>
                   <button type="button" onClick={clearEverything} className="btn">Reset</button>
+                  <button type="button" className="btn" onClick={destroySelf}>Remove</button>
              </div>
         
     </form>
