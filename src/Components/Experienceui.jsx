191c63fdf88251dc26ce9c46e891fcd5db498e14
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-04-28 19:38:10 -0400

    final version of Experienceui.jsx

--- src/Components/Experienceui.jsx
+++ src/Components/Experienceui.jsx
@@ -2,3 +2,19 @@
 
 
 import { useState } from "react";
+
+
+
+
+
+export default function Experienceui({obj}){
+    let bulletPoints = obj.description.split(".");
+    return <div className="workinfo" style={obj.style}>
+    <h3>{obj.title}</h3>
+    <p>{obj.companyName}</p>
+    <p>{obj.startDate} / {obj.endDate}</p>
+    <ul>
+        {bulletPoints.map((point, index) => index < bulletPoints.length - 1 ? <li>{point + "."}</li> : null)}
+    </ul>
+</div>
+}
