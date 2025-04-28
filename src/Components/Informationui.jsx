35ac7d9f9ff44b49a33d3210270d849c8ed65c85
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-04-28 19:39:24 -0400

    final version of Informationui.jsx

--- src/Components/Informationui.jsx
+++ src/Components/Informationui.jsx
@@ -11,7 +11,7 @@
 
 
 export default function Informationui({obj}){
-    return obj == null ? <div className="infosect"><h1>Information</h1><h1>Enter and Save to Show</h1></div> : <div className="infosect">
+    return obj == null ? <div className="infosect"><h1>Enter and Save to Show</h1></div> : <div className="infosect">
         <h1 className="fullName">{obj.firstName + " " + obj.lastName}</h1>
         <div className="otherInfo">
             <p>{obj.phoneNumber}</p>
