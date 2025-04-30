697026c396a466cf8597de2285c8825ed644a7e6
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-04-29 22:29:03 -0400

    changed the way description attribute is achieved, using new line character this time.

--- src/Components/Experienceui.jsx
+++ src/Components/Experienceui.jsx
@@ -1,20 +1,20 @@
 
 
 
-import { useState } from "react";
+
 
 
 
 
 
 export default function Experienceui({obj}){
-    let bulletPoints = obj.description.split(".");
+    let bulletPoints = obj.description.split("\n");
     return <div className="workinfo" style={obj.style}>
     <h3>{obj.title}</h3>
     <p>{obj.companyName}</p>
     <p>{obj.startDate} / {obj.endDate}</p>
     <ul>
-        {bulletPoints.map((point, index) => index < bulletPoints.length - 1 ? <li>{point + "."}</li> : null)}
+        {bulletPoints.map((point) => <li>{point}</li>)}
     </ul>
 </div>
 }
