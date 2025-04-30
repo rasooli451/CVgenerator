e7b0eb211c287f140977cbd13155c12c8d5ade84
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-04-29 22:29:59 -0400

    edited placeholder for the textarea to make it more descriptive

--- src/Components/Projectui.jsx
+++ src/Components/Projectui.jsx
@@ -6,14 +6,14 @@
 
 
 export default function Projectui({obj}){
-    let bulletPoints = obj.description.split(".");
+    let bulletPoints = obj.description.split("\n");
     return <div className="projectinfo" style={obj.style}>
     <div className="row">
         <h3>{obj.projectName + " - "}<i>{obj.tools}</i></h3>
         <a href={obj.link}>View Project</a>
     </div>
     <ul>
-        {bulletPoints.map((point, index) => index < bulletPoints.length - 1 ? <li>{point + "."}</li> : null)}
+        {bulletPoints.map((point) => <li>{point}</li>)}
     </ul>
 </div>
 }
