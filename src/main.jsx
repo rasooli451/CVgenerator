558dc82921592a88b2bc76c8e6aa29d5dd7bb206
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-04-29 22:40:36 -0400

    removed a bad import statement

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Top from './Top.jsx'
-import Information from './Components/Information.jsx'
 
 
 
